9ece60638c07a78c9655c663d29863abf87d7835
devansh <devansh@devanshs-MacBook-Air.local>
2026-08-14 11:43:34 +0530

    Add Day 5 Task 2: container image from dotnet publish with no Dockerfile

---
+++ day-5/task-2/QuotesApi/Data/Author.cs
@@ -0,0 +1,8 @@
+namespace QuotesApi.Data;
+
+public class Author
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public List<Book> Books { get; set; } = [];
+}
